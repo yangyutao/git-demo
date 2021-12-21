5f287ce040e723c965b1338b9bd8061eafc693f7
potato <ytsmallpotato@qq.com>
2021-12-21 16:15:21 +0800

    test 冲突演示

--- demo1/src/com/yt/Test2.java
+++ demo1/src/com/yt/Test2.java
@@ -3,5 +3,6 @@ package com.yt;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("hello git test ");
+        System.out.println("test 冲突演示");
     }
 }
