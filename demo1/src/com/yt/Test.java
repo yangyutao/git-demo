b42a7ef2b9e8aa7d785c15dc4387718411abcf58
potato <ytsmallpotato@qq.com>
2021-12-21 15:34:35 +0800

    尝试第二次提交和推送

--- demo1/src/com/yt/Test.java
+++ demo1/src/com/yt/Test.java
@@ -1,5 +1,11 @@
 package com.yt;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
 
+        System.out.println("hello master");
+
+        System.out.println("hello test");
+    }
 }
